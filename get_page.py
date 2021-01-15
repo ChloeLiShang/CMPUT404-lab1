072486efe69dc50671797f2eec6839206c2a0911
LiShang99 <lshang@ualberta.ca>
2021-01-15 00:19:13 -0700

    print out source code from GitHub

--- get_page.py
+++ get_page.py
@@ -1,3 +1,5 @@
 import requests
 
-page = requests.get("http://www.google.com/")
+page = requests.get("https://raw.githubusercontent.com/ChloeLiShang/CMPUT404-lab1/master/get_page.py")
+
+print(page.content)
